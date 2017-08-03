a6e5c75c364cfa7d8d77e10625fed1042f672e32
DEVOPS\Jason <romeroagency@outlook.com>
2017-08-03 09:09:30 -0400

    update form validation.

---
+++ index.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for index
+/// </summary>
+public class index
+{
+    public index()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
